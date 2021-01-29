e7d6f117d55982a7e29d9c16fc5cee631e48dd1a
Daniel Esteban Gonzalez Zuluaga <danygonza961201@gmail.com>
2021-01-29 13:56:39 -0500

    Implementación de aproximación binaria

--- Raiz cuadrada.py
+++ Raiz cuadrada.py
@@ -1,12 +1,16 @@
 objetivo = int(input('Escoge un numero: '))
-epsilon = 0.01
-paso = epsilon**2 
-respuesta = 0.0
+epsilon = 0.001
+bajo = 0.0
+alto = max(1.0, objetivo)
+respuesta = (alto + bajo) / 2
 
-while respuesta**2 < objetivo and respuesta <= objetivo:
-    respuesta += paso
+while abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'bajo={bajo}, alto={alto}, respuesta={respuesta}')
+    if respuesta**2 < objetivo:
+        bajo = respuesta
+    else:
+        alto = respuesta
 
-if abs(respuesta**2 - objetivo) >= epsilon:
-    print(f'No se encontro la raiz cuadrada {objetivo}')
-else:
-    print(f'La raiz cudrada de {objetivo} es {respuesta}')
+    respuesta = (alto + bajo) / 2
+
+print(f'La raiz cuadrada de {objetivo} es {respuesta}')
