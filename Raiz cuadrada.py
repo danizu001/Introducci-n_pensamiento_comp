40216605898a264065d3afb74ac8743457cdf352
Daniel Esteban Gonzalez Zuluaga <danygonza961201@gmail.com>
2021-01-29 13:25:53 -0500

    Aproximación de raiz cuadrada

--- Raiz cuadrada.py
+++ Raiz cuadrada.py
@@ -1,10 +1,12 @@
-objetivo = int(input("Escoge un entero: "))
-respuesta = 0
+objetivo = int(input('Escoge un numero: '))
+epsilon = 0.01
+paso = epsilon**2 
+respuesta = 0.0
 
-while respuesta**2<objetivo:
-    respuesta += 1
-    
-if respuesta**2 == objetivo:
-        print("La raíz es: " + str(respuesta))
-else: 
-        print("La raíz no es exacta")
+while respuesta**2 < objetivo and respuesta <= objetivo:
+    respuesta += paso
+
+if abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'No se encontro la raiz cuadrada {objetivo}')
+else:
+    print(f'La raiz cudrada de {objetivo} es {respuesta}')
