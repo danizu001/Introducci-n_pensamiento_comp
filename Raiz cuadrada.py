2a692a2f18cdedc8f2cba81c054de366de18c97c
Daniel Esteban Gonzalez Zuluaga <danygonza961201@gmail.com>
2021-01-29 14:51:00 -0500

    Agregando en función sqrt Aproximación

--- Raiz cuadrada.py
+++ Raiz cuadrada.py
@@ -11,6 +11,17 @@ def sqrtBinario(objetivo, epsilon=0.01):
         respuesta = (alto + bajo) / 2
     print(f'La raiz cuadrada de {objetivo} es {respuesta}')
 
+def sqrtAprox(objetivo, epsilon=0.01):
+    paso=epsilon**2
+    respuesta=0
+    while respuesta**2 < objetivo and respuesta <= objetivo:
+        respuesta += paso    
+    if abs(respuesta**2 - objetivo) >= epsilon:
+        print(f'No se encontro la raiz cuadrada {objetivo}')
+    else:
+        print(f'La raiz cudrada de {objetivo} es {respuesta}')
+
 objetivo = int(input('Escoge un numero: '))
 
 sqrtBinario(objetivo)
+sqrtAprox(objetivo)
